47c66bbc3acaff6a998b1e42e225cb33c75dd1c6
Manmeet Dhingra <manmeetdhingra@googlemail.com>
2023-12-30 01:52:43 +0000

    cardsDone

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -58,11 +58,11 @@ export default function Homepage() {
     </div>
     <div className='goals-container'>
       <h2>Our Goals</h2>
-      <div style={{display: "flex", justifyContent: "space-evenly"}}>
-      <p>Informative Content: <br/><br/>Delivering accurate, timely news, updates, and information about local events, weather, traffic, and community affairs to keep listeners informed.</p>
+      <div className='card-container' style={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap"}}>
+      <p>Informative Content: <br/><br/>Committed to ensuring our audience stays well-informed by delivering up-to-the-minute news, information regarding local events, precise weather forecasts, and insightful coverage of community affairs.</p>
       <p>Entertainment and Cultural Representation: <br/><br/> Offering diverse programming that reflects the local culture, interests, and tastes of the community while providing entertainment, music, and content that resonates with local listeners.</p>
       <p>Community Engagement: <br/><br/> Fostering a sense of community by promoting local events, supporting local causes, and providing a platform for community members to share their stories and voices.</p>
-      
+      <p>Supporting Local Businesses: <br/><br/> Promoting and championing the growth of local businesses through strategic advertising, mutually beneficial partnerships, and community-focused initiatives that drive economic vitality and sustainability.</p>
       </div>
     </div>
   </>
