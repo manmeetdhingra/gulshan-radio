2110f0a1ecfa01524f2cee4bf0df3bce73bac2ce
Manmeet Dhingra <manmeetdhingra@googlemail.com>
2023-12-31 21:12:00 +0000

    gallery

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,11 +3,12 @@ import './About.css'
 
 export default function About() {
   return (
-    <div>
-    <h2 style={{marginLeft: "50%"}}>About Us</h2>
+    <div style={{display: "flex", flexDirection: "column", textAlign: "center"}}>
+    <h2 style={{fontSize: "45px",  }}>About Us</h2>
     <div className='about-container'>
       <img src="public/console.jpeg" alt="radio-console" />
       <p>Gulshan Radio is the first and only Asian radio station in the city of Wolverhampton (UK) broadcasting with a significantly large Punjabi listenership. This listenership primarily belongs to the Doaba region of Punjab. Wolverhampton has the second highest percentage of Sikh residents living in England, as per the 2011 census with the first Gurdwara built in 1966. Wolverhampton also holds an annual Vaisakhi mela with thousands of people attending the event. <br/><br/>Gulshan Radio can be heard on 106.9FM across Wolverhampton and surrounding areas broadcasting in Punjabi, Hindi, Urdu and English languages with content that reflects the interests, needs, richness and diversity of the communities we serve.<br/><br/>Gulshan Radio provides local, national and international news updates throughout the day, every day. We provide hourly news updates in Punjabi with special news reports with the latest happenings in Punjab. Gulshan Radio has wide-ranging support from a host of local community organisations, businesses, charities and the public. We also have backing from MPs, councillors and religious institutions.</p>
-    </div></div>
+    </div>
+    </div>
   )
 }
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,10 +1,113 @@
 import React from 'react'
 import "./Gallery.css"
+import ReactImageGallery from 'react-image-gallery'
+
+const images = [
+  {
+    original: "public/Ammy.jpg",
+    thumbnail: "public/Ammy.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Anmol.jpg",
+    thumbnail: "public/Anmol.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Dilpreet.jpg",
+    thumbnail: "public/Dilpreet.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  
+  {
+    original: "public/Saleem.jpg",
+    thumbnail: "public/Saleem.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Gulshan.jpg",
+    thumbnail: "public/Gulshan.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Hard-Kaur.jpg",
+    thumbnail: "public/Hard-Kaur.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Jasbir.jpg",
+    thumbnail: "public/Jasbir.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Jenny-Johal.jpg",
+    thumbnail: "public/Jenny-Johal.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  
+  {
+    original: "public/Lakhwinder-Wadali.jpg",
+    thumbnail: "public/Lakhwinder-Wadali.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Malkit Singh.jpg",
+    thumbnail: "public/Malkit Singh.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Mankirat.jpg",
+    thumbnail: "public/Mankirat.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Ninja.jpg",
+    thumbnail: "public/Ninja.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Roshan-prince.jpg",
+    thumbnail: "public/Roshan-prince.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  
+  {
+    original: "public/Sharry-Mann.jpg",
+    thumbnail: "public/Sharry-Mann.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Surinder-Shinda.jpg",
+    thumbnail: "public/Surinder-Shinda.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+  {
+    original: "public/Surjit Khan.jpg",
+    thumbnail: "public/Surjit Khan.jpg",
+    originalHeight: "400px",
+    thumbnailHeight: "65px"
+  },
+]
 
 export default function Gallery() {
   return (
-    <div>
-      Gallery
+    <div className='gallery-container'>
+      <ReactImageGallery items={images} showBullets={true} showThumbnails={true} showPlayButton={false} showFullscreenButton={true}/>
     </div>
   )
 }
