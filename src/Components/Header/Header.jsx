49e190af0b87b84fd5287b85be97380f3f990bd7
Manmeet Dhingra <manmeetdhingra@googlemail.com>
2024-03-24 13:35:26 +0000

    livebutton

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,21 +16,18 @@ export default function Header() {
     <div style={{display: "flex", justifyContent: "space-between", width: 'auto'}} className='header-container'>
         <div><img src='/GulshanRadio-UPDATE3-03(1).png' onClick={() => navigate("/")} style={{cursor: "pointer", marginLeft: "5px"}}/></div>
         <div className={`button-container ${isOpen && "open"}`} >
-          {/* <NavLink to={"/"} className={({isActive}) => {isActive && "active"}}>
-            Home
-          </NavLink>
-          <NavLink to={"/schedule"} className={({isActive, isPending}) => {isPending ? "" : isActive ? "active" : ""}}>
-            Schedule
-          </NavLink> */}
         <button onClick={() => {setIsOpen(false); navigate("/")}}>Home</button>
         <button onClick={() => {setIsOpen(false); navigate("/schedule")}}>Schedule</button>
         <button onClick={() => {setIsOpen(false); navigate("/presenters")}}>Presenters</button>
         <button onClick={() => {setIsOpen(false); navigate("/gallery")}}>Gallery</button>
         <button onClick={() => {setIsOpen(false); navigate("/about")}}>About Us</button>
         <button onClick={() => {setIsOpen(false); navigate("/contact")}}>Contact Us</button>
-        
+
         <a onClick={() => setIsOpen(false)} href="/GR-MediaPack.pdf" download={"GR-MediaPack.pdf"}><button>Media Pack</button></a>
         </div>
+
+        <div className='live'><a onClick={() => setIsOpen(false)} href="https://radio-live-uk.com/gulshan" target='blank'><button>Listen LIVE</button></a></div>
+
         <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => {setIsOpen(!isOpen)}}>
           <div className="bar">
           </div>
